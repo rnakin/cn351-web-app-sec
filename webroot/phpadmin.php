e1a19d2428cbe4c523739fc28c9f22c7b36cdff9
Ploypan Tengprayoon <6510615245@cn102>
2025-06-13 10:25:56 +0700

    Change db_name

--- webroot/phpadmin.php
+++ webroot/phpadmin.php
@@ -4,7 +4,7 @@ mysqli_report(MYSQLI_REPORT_STRICT|MYSQLI_REPORT_ERROR);
 $host = "mariadb";
 $username = "db_user";
 $password = "db_pw";
-$dbname = "";
+$dbname = "db_test";
 
 try {
     return mysqli_connect($host, $username, $password, $dbname);
